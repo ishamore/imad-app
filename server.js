71f7e0d2d334b8699e1ece180cfb6eae457d1596
ishamore <30793042+ishamore@users.noreply.github.com>
2017-08-07 17:08:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('article-one', function(req, res){
-    res.sendFile("This is article one");
+    res.send("This is article one");
 });
 
 app.get('article-two', function(req, res){
-    res.sendFile("This is article two");
+    res.send("This is article two");
 });
 
 app.get('article-three', function(req, res){
-    res.sendFile("This is article three");
+    res.send("This is article three");
 });
 
 app.get('/ui/style.css', function (req, res) {
