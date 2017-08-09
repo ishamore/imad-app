d1c665781741925e5e611dec8d63e82101debea2
ishamore <30793042+ishamore@users.noreply.github.com>
2017-08-09 23:23:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,28 @@
 console.log('Loaded!');
+//counter code
+
+var button = document.getElementById('counter');
+
+button.onclick = function() {
+    //create a request object
+    
+    var request = new XMLHttpRequest();
+    
+    //capture a request and store it
+    
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+    };
+    
+    request.open('GET','http://ishamore2305.imad.hasura-app.io/counter',true);
+    request.send(null);
+}
