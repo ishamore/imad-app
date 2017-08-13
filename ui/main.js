d895235a054b6b69ce3a4c7964457308f1332f2a
ishamore <30793042+ishamore@users.noreply.github.com>
2017-08-13 19:50:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,27 +2,34 @@
 //counter code
 
 var button = document.getElementById('counter');
+var counter = 0;
 
 button.onclick = function() {
     //create a request object
     
-    var request = new XMLHttpRequest();
+    // var request = new XMLHttpRequest();
     
-    //capture a request and store it
+    // //capture a request and store it
     
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE)
-        {
-            if(request.status===200)
-            {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+    // request.onreadystatechange = function() {
+    //     if(request.readyState === XMLHttpRequest.DONE)
+    //     {
+    //         if(request.status===200)
+    //         {
+    //             var counter = request.responseText;
+    //             var span = document.getElementById('count');
+    //             span.innerHTML = counter.toString();
                 
-            }
-        }
-    };
+    //         }
+    //     }
+    // };
     
-    request.open('GET','http://ishamore2305.imad.hasura-app.io/counter',true);
-    request.send(null);
+    // request.open('GET','http://ishamore2305.imad.hasura-app.io/counter',true);
+    // request.send(null);
+    
+    //Render the variable in correct span
+    
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 }
