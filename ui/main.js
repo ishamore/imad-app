8c143d474f02001eb993a648797f1b02263e198b
ishamore <30793042+ishamore@users.noreply.github.com>
2017-08-13 20:04:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
-// console.log('Loaded!');
+
 //counter code
 
-var button = document.getElementById('counter');
+var button = document.getElementById("counter");
 var counter = 0;
 
 button.onclick = function() {
@@ -30,6 +30,6 @@ button.onclick = function() {
     //Render the variable in correct span
     
     counter = counter + 1;
-    var span = document.getElementById('count');
+    var span = document.getElementById("count");
     span.innerHTML = counter.toString();
 };
